fa57160697a5616303a17eeba1e19ff236ba029d
dotvirus <33938500+dotvirus@users.noreply.github.com>
2020-08-28 15:30:37 +0200

    PUT queue, POST enqueue, POST dequeue

--- src/main.rs
+++ src/main.rs
@@ -8,26 +8,22 @@ mod queue;
 use chrono::{DateTime, Local};
 use lazy_static::lazy_static;
 use nickel::status::StatusCode;
-use nickel::{HttpRouter, Nickel};
+use nickel::{HttpRouter, JsonBody, Nickel, Request, Response};
 use queue::Queue;
+use serde_json::{json, Value};
 use std::collections::HashMap;
 use std::fs;
 use std::sync::Mutex;
 use std::time::{Instant, SystemTime, UNIX_EPOCH};
 
 #[derive(Serialize, Deserialize)]
-struct RootInfo {
-  name: String,
-  version: String,
-  uptime_ms: u64,
-  uptime_secs: u64,
-  started_at: u64,
+struct EnqueueBody {
+  item: Value,
 }
 
 #[derive(Serialize, Deserialize)]
-struct QueueInfoResponse {
-  name: String,
-  size: u64,
+struct SuccessResponse {
+  result: Value,
 }
 
 #[derive(Serialize, Deserialize)]
@@ -45,6 +41,36 @@ lazy_static! {
   };
 }
 
+fn iso_date() -> String {
+  let now: DateTime<Local> = Local::now();
+  now.to_rfc2822()
+}
+
+fn queue_exists(req: &mut Request) -> bool {
+  let queue_map = QUEUES.lock().unwrap();
+  let queue_name = String::from(req.param("queue_name").unwrap());
+  let queue_maybe = queue_map.get(&queue_name);
+  queue_maybe.is_some()
+}
+
+fn success(result: Value) -> Result<Value, (nickel::status::StatusCode, serde_json::error::Error)> {
+  let res_body = SuccessResponse { result };
+  serde_json::to_value(res_body).map_err(|e| (StatusCode::InternalServerError, e))
+}
+
+fn error(
+  res: &mut Response,
+  status: StatusCode,
+  message: &str,
+) -> Result<Value, (nickel::status::StatusCode, serde_json::error::Error)> {
+  let res_body = ErrorResponse {
+    error: true,
+    message: String::from(message),
+  };
+  res.set(status);
+  serde_json::to_value(res_body).map_err(|e| (StatusCode::InternalServerError, e))
+}
+
 fn main() {
   fs::create_dir("corinth_data").ok();
 
@@ -52,9 +78,8 @@ fn main() {
   let start_time = Instant::now();
 
   // Logger middleware
-  server.utilize(middleware! { |request|
-    let now: DateTime<Local> = Local::now();
-    println!("{} {}: {}", request.origin.method.to_string(), request.origin.uri.to_string(), now.to_rfc2822());
+  server.utilize(middleware! { |req|
+    println!("{} {}: {}", req.origin.method.to_string(), req.origin.uri.to_string(), iso_date());
   });
 
   // Get server info
@@ -63,14 +88,13 @@ fn main() {
     middleware! { |_req|
       let now = SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_secs();
       let uptime_secs = start_time.elapsed().as_secs();
-      let res_body = RootInfo {
-        name: String::from("Corinth"),
-        version: String::from("0.0.1"),
-        uptime_ms: uptime_secs * 1000,
-        uptime_secs,
-        started_at: now - uptime_secs,
-      };
-      serde_json::to_value(res_body).map_err(|e| (StatusCode::InternalServerError, e))
+      serde_json::to_value(json!({
+        "name": String::from("Corinth"),
+        "version": String::from("0.0.1"),
+        "uptime_ms": uptime_secs * 1000,
+        "uptime_secs": uptime_secs,
+        "started_at": now - uptime_secs,
+      })).map_err(|e| (StatusCode::InternalServerError, e))
     },
   );
 
@@ -78,32 +102,85 @@ fn main() {
   server.get(
     "/queue/:queue_name",
     middleware! { |req, mut res|
-      let queue_map = QUEUES.lock().unwrap();
-      let queue_name = String::from(req.param("queue_name").unwrap());
-      let queue_maybe = queue_map.get(&queue_name);
-
-      if queue_maybe.is_some() {
-        let queue = queue_maybe.unwrap();
-        let res_body = QueueInfoResponse {
-          name: queue_name.clone(),
-          size: queue.size() as u64,
-        };
-        serde_json::to_value(res_body).map_err(|e| (StatusCode::InternalServerError, e))
+      if queue_exists(req) {
+        let queue_name = String::from(req.param("queue_name").unwrap());
+        let queue_map = QUEUES.lock().unwrap();
+        let queue = queue_map.get(&queue_name).unwrap();
+        success(json!({
+          "name": queue_name,
+          "size": queue.size()
+        }))
       }
       else {
-        let res_body = ErrorResponse {
-          error: true,
-          message: String::from("Queue not found"),
-        };
-        res.set(StatusCode::NotFound);
-        serde_json::to_value(res_body).map_err(|e| (StatusCode::InternalServerError, e))
+        error(&mut res, StatusCode::NotFound, "Queue not found")
       }
     },
   );
 
-  // server.put(
-  //   "/queue/:queue_name/:id"
-  // );
+  server.post(
+    "/queue/:queue_name/enqueue",
+    middleware! { |req, mut res|
+      let body = try_with!(res, {
+          req.json_as::<EnqueueBody>().map_err(|e| (StatusCode::BadRequest, e))
+      });
+      if body.item.is_object() {
+        if queue_exists(req) {
+          let mut queue_map = QUEUES.lock().unwrap();
+          let queue = queue_map.get_mut(&String::from(req.param("queue_name").unwrap())).unwrap();
+          queue.enqueue(body.item);
+          success(json!({
+            "message": "Message enqueued"
+          }))
+        }
+        else {
+          // TODO: check ?create_queue=true
+          error(&mut res, StatusCode::NotFound, "Queue not found")
+        }
+      }
+      else {
+        error(&mut res, StatusCode::BadRequest, "body.item is required to be of type 'object'")
+      }
+    },
+  );
+
+  server.post(
+    "/queue/:queue_name/dequeue",
+    middleware! { |req, mut res|
+      if queue_exists(req) {
+        let mut queue_map = QUEUES.lock().unwrap();
+        let queue = queue_map.get_mut(&String::from(req.param("queue_name").unwrap())).unwrap();
+        let message = queue.dequeue();
+        if message.is_some() {
+          success(json!({
+            "message": message.unwrap()
+          }))
+        }
+        else {
+          res.set(StatusCode::NoContent);
+          return res.send("");
+        }
+      }
+      else {
+        error(&mut res, StatusCode::NotFound, "Queue not found")
+      }
+    },
+  );
+
+  server.put(
+    "/queue/:queue_name",
+    middleware! { |req, mut res|
+      if queue_exists(req) {
+        error(&mut res, StatusCode::Conflict, "Queue already exists")
+      }
+      else {
+        let mut queue_map = QUEUES.lock().unwrap();
+        let queue_name = String::from(req.param("queue_name").unwrap());
+        queue_map.insert(queue_name, Queue::new());
+        res.set(StatusCode::Created);
+        success(json!(null))
+      }
+    },
+  );
 
   // Parse port from cli arguments
   let mut port = 6767;
--- src/queue.rs
+++ src/queue.rs
@@ -1,8 +1,15 @@
+use oysterpack_uid::ulid::ulid_str;
 use serde_json::Value;
 use std::collections::VecDeque;
 
+#[derive(Serialize, Deserialize)]
+pub struct Message {
+  id: String,
+  pub item: Value,
+}
+
 pub struct Queue {
-  items: VecDeque<Value>,
+  items: VecDeque<Message>,
 }
 
 impl Queue {
@@ -10,6 +17,15 @@ impl Queue {
     return Queue {
       items: VecDeque::new(),
     };
+  }
+
+  pub fn enqueue(&mut self, item: Value) {
+    let id = ulid_str();
+    self.items.push_back(Message { id, item });
+  }
+
+  pub fn dequeue(&mut self) -> Option<Message> {
+    self.items.pop_front()
   }
 
   pub fn size(&self) -> usize {
