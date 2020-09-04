264a2957e78d15ee2a08e16dca454dadb3deddda
dotvirus <33938500+dotvirus@users.noreply.github.com>
2020-09-04 22:32:43 +0200

    refactor create_queue

--- test/tests/create_queue.test.ts
+++ test/tests/create_queue.test.ts
@@ -1,11 +1,8 @@
 import ava from "ava";
 import Axios from "axios";
 import { spawnCorinth, NO_FAIL } from "../util";
-import {
-  queueUrl as getQueueUrl,
-  createQueue,
-  validateEmptyQueueResponse,
-} from "../common";
+import { queueUrl as getQueueUrl, createQueue } from "../common";
+import yxc, { createExecutableSchema } from "@dotvirus/yxc";
 
 spawnCorinth();
 
@@ -14,21 +11,71 @@ const queueUrl = getQueueUrl(queueName);
 
 ava.serial("Queue shouldn't exist", async (t) => {
   const res = await Axios.get(queueUrl, NO_FAIL());
-  t.is(res.status, 404);
-  t.deepEqual(res.data, {
-    error: true,
-    message: "Queue not found",
-  });
+  t.deepEqual(
+    createExecutableSchema(
+      yxc
+        .object({
+          status: yxc.number().enum([404]),
+          data: yxc.object({
+            error: yxc.boolean().true(),
+            message: yxc.string().enum(["Queue not found"]),
+          }),
+        })
+        .arbitrary()
+    )(res),
+    []
+  );
 });
 
 ava.serial("Create queue", async (t) => {
   const res = await createQueue(queueName, NO_FAIL());
-  t.is(res.status, 201);
-  t.is(res.data.message, "Queue created successfully");
+  t.deepEqual(
+    createExecutableSchema(
+      yxc
+        .object({
+          status: yxc.number().enum([201]),
+          data: yxc.object({
+            message: yxc.string().enum(["Queue created successfully"]),
+            result: yxc
+              .any()
+              .nullable()
+              .use((v) => v === null),
+          }),
+        })
+        .arbitrary()
+    )(res),
+    []
+  );
 });
 
 ava.serial("Queue should be empty", async (t) => {
   const res = await Axios.get(queueUrl, NO_FAIL());
-  t.is(res.status, 200);
-  validateEmptyQueueResponse(t, queueName, res);
+  t.deepEqual(
+    createExecutableSchema(
+      yxc
+        .object({
+          status: yxc.number().enum([200]),
+          data: yxc.object({
+            message: yxc.string().enum(["Queue info retrieved successfully"]),
+            result: yxc.object({
+              queue: yxc.object({
+                name: yxc.string().enum([queueName]),
+                created_at: yxc.number().integer(),
+                size: yxc.number().enum([0]),
+                num_deduped: yxc.number().enum([0]),
+                num_unacked: yxc.number().enum([0]),
+                num_dedup_hits: yxc.number().enum([0]),
+                num_acknowledged: yxc.number().enum([0]),
+                dedup_time: yxc.number().enum([300]),
+                ack_time: yxc.number().enum([300]),
+                persistent: yxc.boolean().false(),
+                mem_size: yxc.number(),
+              }),
+            }),
+          }),
+        })
+        .arbitrary()
+    )(res),
+    []
+  );
 });
