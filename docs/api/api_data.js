0200010fc1ee3b458b40262a9b2c2e09465f88c7
dotvirus <33938500+dotvirus@users.noreply.github.com>
2020-09-09 18:19:15 +0200

    docs

---
+++ docs/api/api_data.js
@@ -0,0 +1,494 @@
+define({ "api": [
+  {
+    "type": "get",
+    "url": "/",
+    "title": "Get server info",
+    "name": "RootInfo",
+    "group": "Misc",
+    "success": {
+      "fields": {
+        "Success 200": [
+          {
+            "group": "Success 200",
+            "type": "String",
+            "optional": false,
+            "field": "result:info:name",
+            "description": "<p>Server name</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "String",
+            "optional": false,
+            "field": "result:info:version",
+            "description": "<p>Server version</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "Number",
+            "optional": false,
+            "field": "result:info:uptime_ms",
+            "description": "<p>Uptime in milliseconds</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "Number",
+            "optional": false,
+            "field": "result:info:uptime_secs",
+            "description": "<p>Uptime in seconds</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "Number",
+            "optional": false,
+            "field": "result:info:started_at",
+            "description": "<p>Unix timestamp when the server was started</p>"
+          }
+        ]
+      }
+    },
+    "version": "0.0.0",
+    "filename": "src/server.rs",
+    "groupTitle": "Misc"
+  },
+  {
+    "type": "post",
+    "url": "/queue/:queue/:message/ack",
+    "title": "Acknowledge message reception",
+    "name": "AckMessage",
+    "group": "Queue",
+    "error": {
+      "fields": {
+        "Error 4xx": [
+          {
+            "group": "Error 4xx",
+            "optional": false,
+            "field": "404",
+            "description": "<p>Message not found</p>"
+          }
+        ]
+      }
+    },
+    "version": "0.0.0",
+    "filename": "src/server.rs",
+    "groupTitle": "Queue"
+  },
+  {
+    "type": "put",
+    "url": "/queue/:queue",
+    "title": "Create queue",
+    "name": "CreateQueue",
+    "group": "Queue",
+    "parameter": {
+      "fields": {
+        "Parameter": [
+          {
+            "group": "Parameter",
+            "type": "String",
+            "optional": false,
+            "field": "query:requeue_time",
+            "description": "<p>(Optional) Ack time in seconds</p>"
+          },
+          {
+            "group": "Parameter",
+            "type": "String",
+            "optional": false,
+            "field": "query:deduplication_time",
+            "description": "<p>(Optional) Deduplication time in seconds</p>"
+          },
+          {
+            "group": "Parameter",
+            "type": "String",
+            "optional": false,
+            "field": "query:persistent",
+            "description": "<p>(Optional) Set to &quot;true&quot; to make queue persistent</p>"
+          }
+        ]
+      }
+    },
+    "error": {
+      "fields": {
+        "Error 4xx": [
+          {
+            "group": "Error 4xx",
+            "optional": false,
+            "field": "400",
+            "description": "<p>Invalid time argument</p>"
+          },
+          {
+            "group": "Error 4xx",
+            "optional": false,
+            "field": "409",
+            "description": "<p>Queue already exists</p>"
+          }
+        ]
+      }
+    },
+    "version": "0.0.0",
+    "filename": "src/server.rs",
+    "groupTitle": "Queue"
+  },
+  {
+    "type": "delete",
+    "url": "/queue/:queue",
+    "title": "Delete queue",
+    "name": "DeleteQueue",
+    "group": "Queue",
+    "error": {
+      "fields": {
+        "Error 4xx": [
+          {
+            "group": "Error 4xx",
+            "optional": false,
+            "field": "404",
+            "description": "<p>Queue not found</p>"
+          }
+        ]
+      }
+    },
+    "version": "0.0.0",
+    "filename": "src/server.rs",
+    "groupTitle": "Queue"
+  },
+  {
+    "type": "post",
+    "url": "/queue/:queue/dequeue",
+    "title": "Dequeue message(s)",
+    "name": "DequeueMessages",
+    "group": "Queue",
+    "parameter": {
+      "fields": {
+        "Parameter": [
+          {
+            "group": "Parameter",
+            "type": "String",
+            "optional": false,
+            "field": "query:ack",
+            "description": "<p>(Optional) Set to &quot;true&quot; to automatically acknowledge message(s)</p>"
+          },
+          {
+            "group": "Parameter",
+            "type": "String",
+            "optional": false,
+            "field": "query:amount",
+            "description": "<p>(Optional) Amount of items to dequeue and return</p>"
+          }
+        ]
+      }
+    },
+    "success": {
+      "fields": {
+        "Success 200": [
+          {
+            "group": "Success 200",
+            "type": "Array",
+            "optional": false,
+            "field": "result:items",
+            "description": "<p>Dequeued messages</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "Array",
+            "optional": false,
+            "field": "result:num_items",
+            "description": "<p>Amount of dequeued messages</p>"
+          }
+        ]
+      }
+    },
+    "error": {
+      "fields": {
+        "Error 4xx": [
+          {
+            "group": "Error 4xx",
+            "optional": false,
+            "field": "400",
+            "description": "<p>Invalid amount parameter</p>"
+          },
+          {
+            "group": "Error 4xx",
+            "optional": false,
+            "field": "404",
+            "description": "<p>Queue not found</p>"
+          }
+        ]
+      }
+    },
+    "version": "0.0.0",
+    "filename": "src/server.rs",
+    "groupTitle": "Queue"
+  },
+  {
+    "type": "post",
+    "url": "/queue/:queue/enqueue",
+    "title": "Enqueue message(s)",
+    "name": "EnqueueMessages",
+    "group": "Queue",
+    "parameter": {
+      "fields": {
+        "Parameter": [
+          {
+            "group": "Parameter",
+            "type": "Array",
+            "optional": false,
+            "field": "body:messages",
+            "description": "<p>List of messages: { item: Object, deduplication: Nullable<String> }</p>"
+          },
+          {
+            "group": "Parameter",
+            "type": "String",
+            "optional": false,
+            "field": "query:create_queue",
+            "description": "<p>(Optional) Set to &quot;true&quot; to create queue if not found</p>"
+          },
+          {
+            "group": "Parameter",
+            "type": "String",
+            "optional": false,
+            "field": "query:persistent_queue",
+            "description": "<p>(Optional) Set to &quot;true&quot; to make created queue persistent</p>"
+          }
+        ]
+      }
+    },
+    "success": {
+      "fields": {
+        "Success 200": [
+          {
+            "group": "Success 200",
+            "type": "Number",
+            "optional": false,
+            "field": "result:num_enqueued",
+            "description": "<p>Amount of enqueued messages</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "Number",
+            "optional": false,
+            "field": "result:num_deduplicated",
+            "description": "<p>Amount of deduplicated messages</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "Array",
+            "optional": false,
+            "field": "result:items",
+            "description": "<p>Created messages</p>"
+          }
+        ]
+      }
+    },
+    "error": {
+      "fields": {
+        "Error 4xx": [
+          {
+            "group": "Error 4xx",
+            "optional": false,
+            "field": "400",
+            "description": "<p>Bad Request (malformed body)</p>"
+          },
+          {
+            "group": "Error 4xx",
+            "optional": false,
+            "field": "404",
+            "description": "<p>Queue not found</p>"
+          }
+        ]
+      }
+    },
+    "version": "0.0.0",
+    "filename": "src/server.rs",
+    "groupTitle": "Queue"
+  },
+  {
+    "type": "get",
+    "url": "/queues",
+    "title": "List queues",
+    "name": "ListQueues",
+    "group": "Queue",
+    "success": {
+      "fields": {
+        "Success 200": [
+          {
+            "group": "Success 200",
+            "type": "String",
+            "optional": false,
+            "field": "result:queues:items",
+            "description": "<p>Queue names</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "Number",
+            "optional": false,
+            "field": "result:queues:length",
+            "description": "<p>Amount of queues</p>"
+          }
+        ]
+      }
+    },
+    "version": "0.0.0",
+    "filename": "src/server.rs",
+    "groupTitle": "Queue"
+  },
+  {
+    "type": "get",
+    "url": "/queues",
+    "title": "Get queue info",
+    "name": "ListQueues",
+    "group": "Queue",
+    "success": {
+      "fields": {
+        "Success 200": [
+          {
+            "group": "Success 200",
+            "type": "String",
+            "optional": false,
+            "field": "result:queue:name",
+            "description": "<p>Queue name</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "Number",
+            "optional": false,
+            "field": "result:queue:created_at",
+            "description": "<p>Queue creation unix timestamp</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "Number",
+            "optional": false,
+            "field": "result:queue:size",
+            "description": "<p>Queue size (length)</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "Number",
+            "optional": false,
+            "field": "result:queue:num_deduplicating",
+            "description": "<p>Amount of tracked deduplication IDs</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "Number",
+            "optional": false,
+            "field": "result:queue:num_unacknowledged",
+            "description": "<p>Amount of unacknowledged messages</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "Number",
+            "optional": false,
+            "field": "result:queue:num_acknowledged",
+            "description": "<p>Amount of acknowledged (done) messages</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "Number",
+            "optional": false,
+            "field": "result:queue:num_deduplicated",
+            "description": "<p>Amount of deduplicated items</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "Number",
+            "optional": false,
+            "field": "result:queue:deduplication_time",
+            "description": "<p>Time for deduplication ID to expire</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "Number",
+            "optional": false,
+            "field": "result:queue:requeue_time",
+            "description": "<p>Time for an unacknowledged message to get added back into the queue</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "String",
+            "optional": false,
+            "field": "result:queue:persistent",
+            "description": "<p>Whether the queue is saved on disk</p>"
+          },
+          {
+            "group": "Success 200",
+            "type": "Number",
+            "optional": false,
+            "field": "result:queue:memory_size",
+            "description": "<p>Approximate memory usage of the queue</p>"
+          }
+        ]
+      }
+    },
+    "error": {
+      "fields": {
+        "Error 4xx": [
+          {
+            "group": "Error 4xx",
+            "optional": false,
+            "field": "404",
+            "description": "<p>Queue not found</p>"
+          }
+        ]
+      }
+    },
+    "version": "0.0.0",
+    "filename": "src/server.rs",
+    "groupTitle": "Queue"
+  },
+  {
+    "type": "get",
+    "url": "/queue/:queue/peek",
+    "title": "Peek queue head",
+    "name": "PeekQueue",
+    "group": "Queue",
+    "success": {
+      "fields": {
+        "Success 200": [
+          {
+            "group": "Success 200",
+            "type": "Array",
+            "optional": false,
+            "field": "result:item",
+            "description": "<p>Queue head</p>"
+          }
+        ]
+      }
+    },
+    "error": {
+      "fields": {
+        "Error 4xx": [
+          {
+            "group": "Error 4xx",
+            "optional": false,
+            "field": "404",
+            "description": "<p>Queue not found</p>"
+          }
+        ]
+      }
+    },
+    "version": "0.0.0",
+    "filename": "src/server.rs",
+    "groupTitle": "Queue"
+  },
+  {
+    "type": "delete",
+    "url": "/queue/:queue/purge",
+    "title": "Purge queue state",
+    "name": "PurgeQueue",
+    "group": "Queue",
+    "error": {
+      "fields": {
+        "Error 4xx": [
+          {
+            "group": "Error 4xx",
+            "optional": false,
+            "field": "404",
+            "description": "<p>Queue not found</p>"
+          }
+        ]
+      }
+    },
+    "version": "0.0.0",
+    "filename": "src/server.rs",
+    "groupTitle": "Queue"
+  }
+] });
