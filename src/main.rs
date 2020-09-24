1727224b7c915295be24dd313edc070634a24d4c
dotvirus <33938500+dotvirus@users.noreply.github.com>
2020-09-24 22:02:48 +0200

    basic benchmark, closes #5 closes #6

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ extern crate nickel;
 #[macro_use]
 extern crate serde_derive;
 
+mod benchmark;
 mod date;
 mod env;
 mod fs;
@@ -17,6 +18,10 @@ use crate::global_data::read_queues_from_disk;
 use crate::server::create_server;
 
 fn main() {
+  if std::env::args().any(|x| x == "--benchmark") {
+    benchmark::benchmark();
+  }
+
   read_queues_from_disk();
   create_server()
     .listen(format!("127.0.0.1:{}", get_port()))
