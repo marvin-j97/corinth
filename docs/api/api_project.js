18a3ac0bd982f70fee6ca5f19557fecab1e874f8
dotvirus <dotvirus@users.noreply.github.com>
2020-10-12 11:51:17 +0000

    Build API docs

--- docs/api/api_project.js
+++ docs/api/api_project.js
@@ -8,7 +8,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2020-10-09T11:12:15.651Z",
+    "time": "2020-10-12T11:51:16.772Z",
     "url": "https://apidocjs.com",
     "version": "0.25.0"
   }
