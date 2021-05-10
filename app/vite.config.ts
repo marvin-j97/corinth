1a15cf99a26ed70b5491bc1740c129d6d498df1a
marvin-j97 <33938500+marvin-j97@users.noreply.github.com>
2021-05-11 00:38:19 +0200

    Update vite.config.ts

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:44445/",
+        target: "http://localhost:44444/",
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
     },
