6ec03d78af6f38a9040fca9b03070689aabbd734
marvin-j97 <marvin-j97@users.noreply.github.com>
2021-05-17 21:48:19 +0000

    Build API docs

--- docs/api/api_project.js
+++ docs/api/api_project.js
@@ -8,7 +8,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2021-05-17T13:25:11.599Z",
+    "time": "2021-05-17T21:48:17.896Z",
     "url": "https://apidocjs.com",
     "version": "0.26.0"
   }
